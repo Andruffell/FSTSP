8413c54ebe583d8747e74480a99c8bb2089da24d
Andrea Vignali <an.vignali@icloud.com>
2020-07-21 10:47:15 +0200

    Modificato costo in vettore dei tempi di arrivo

--- FSTSP/src/tsp/TSPsolver.java
+++ FSTSP/src/tsp/TSPsolver.java
@@ -4,20 +4,20 @@ import java.util.Stack;
 
 public class TSPsolver {
 	
-	
     private ArrayList<Integer> list;
-    private Integer costo;
+    private ArrayList<Integer> tempiDiArrivo;
+    
     public TSPsolver()
     {
-        costo = 0;
+        tempiDiArrivo = new ArrayList<Integer>();
         list = new ArrayList<Integer>();
     }
     
     public ArrayList<Integer> getList(){return list;}
-    public Integer getCosto() {return costo;}
+    public ArrayList<Integer> getTempiDiArrivo() {return tempiDiArrivo;}
     /** 
      * Questa funzione utilizza un'euristica Greedy di tipo nearest neighbour. 
-     * @param adjacencyMatrix che è una matrice quadrata di interi, simmetrica con diagonale principale nulla, la quale rappresenta
+     * @param adjacencyMatrix che ï¿½ una matrice quadrata di interi, simmetrica con diagonale principale nulla, la quale rappresenta
      * la matrice di adiacenza di un grafo non orientato.
      */
     public void tsp(int adjacencyMatrix[][])
@@ -32,6 +32,7 @@ public class TSPsolver {
         int min = Integer.MAX_VALUE;
         boolean minFlag = false;
         list.add(0);
+        tempiDiArrivo.add(0);
         while (!stack.isEmpty())
         {
             element = stack.peek();
@@ -57,39 +58,37 @@ public class TSPsolver {
             {
                 visited[dst] = 1;
                 stack.push(dst);
-                costo = costo + min;               
+                tempiDiArrivo.add(tempiDiArrivo.get(tempiDiArrivo.size()-1)+min);             
                 list.add(dst);
                 minFlag = false;
                 continue;
             }
             stack.pop();
         }
-        costo += adjacencyMatrix[list.get(list.size()-1)][0];
-        list.add(0);
-        
+        tempiDiArrivo.add(tempiDiArrivo.get(tempiDiArrivo.size()-1) + adjacencyMatrix[list.get(list.size()-1)][0]);
+        list.add(0);  
     }
-    
-    
+        
     
 	public static void main(String[] args) {
-        	/*int adjacency_matrix[][] = {{0,374,200,223,108,178,252,285,240,356},
-        								{374,0,255,166,433,199,135,95,136,17},
-        								{200,255,0,128,277,128,180,160,131,247},
-        								{223,166,128,0,430,47,52,84,40,155},
-        								{108, 433, 277, 430, 0, 453, 478, 344, 389, 423},
-        								{178, 199, 128, 47, 453, 0, 91, 110, 64, 181},
-        								{252, 135, 180, 52, 478, 91, 0, 114, 83, 117},
-        								{285, 95, 160, 84, 344, 110, 114, 0, 47, 78},
-        								{240, 136, 131, 40, 389, 64, 83, 47, 0, 118},
-        								{356, 17, 247, 155, 423, 181, 117, 78, 118, 0}};*/
+//        	int adjacency_matrix[][] = {{0,374,200,223,108,178,252,285,240,356},
+//        								{374,0,255,166,433,199,135,95,136,17},
+//        								{200,255,0,128,277,128,180,160,131,247},
+//        								{223,166,128,0,430,47,52,84,40,155},
+//        								{108, 433, 277, 430, 0, 453, 478, 344, 389, 423},
+//        								{178, 199, 128, 47, 453, 0, 91, 110, 64, 181},
+//        								{252, 135, 180, 52, 478, 91, 0, 114, 83, 117},
+//        								{285, 95, 160, 84, 344, 110, 114, 0, 47, 78},
+//        								{240, 136, 131, 40, 389, 64, 83, 47, 0, 118},
+//        								{356, 17, 247, 155, 423, 181, 117, 78, 118, 0}};
         								
-        	/*int adjacency_matrix[][] = {{0, 10, 15, 20}, 
-
-                    					{10, 0, 35, 25}, 
-
-                    					{15, 35, 0, 30}, 
-
-                    					{20, 25, 30, 0}};*/
+//        	int adjacency_matrix[][] = {{0, 10, 15, 20}, 
+//
+//                    					{10, 0, 35, 25}, 
+//
+//                    					{15, 35, 0, 30}, 
+//
+//                    					{20, 25, 30, 0}};
         	
 		int adjacency_matrix[][] = {{0,5,15,5,7},
         							 {5,0,5,2,3},
@@ -98,14 +97,10 @@ public class TSPsolver {
         							 {7,3,1,3,0}};
         	
            
-            System.out.println("the citys are visited as follows");
             TSPsolver tspNearestNeighbour = new TSPsolver();
             tspNearestNeighbour.tsp(adjacency_matrix);
-            for (Integer is : tspNearestNeighbour.getList()) {
-				System.out.print(is + "\t");
-			}
-            System.out.print("\n");
-            System.out.print("costo :"+ tspNearestNeighbour.getCosto());
+            System.out.println("nodi attraversati: " + tspNearestNeighbour.getList());
+            System.out.println("costo: "+ tspNearestNeighbour.getTempiDiArrivo());
     }
 
 }
