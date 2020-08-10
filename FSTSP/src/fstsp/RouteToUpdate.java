0ef69a2a3f3909dd44e3f8f654d0495d1bc4fb73
Valerio Volpe <valeriovolpe@hotmail.it>
2020-08-10 17:15:54 +0200

    ALTRI COMMENTI
    :)

--- FSTSP/src/fstsp/RouteToUpdate.java
+++ FSTSP/src/fstsp/RouteToUpdate.java
@@ -1,13 +1,22 @@
 package fstsp;
 
 import java.util.ArrayList;
-
+/**
+ * Classe che contiene la route da aggiornare
+ */
 public class RouteToUpdate {
 	public ArrayList<Node> truckRoute;
 	public ArrayList<Subroute> truckSubroutes;
 	public ArrayList<Node> Cprime;
 	public ArrayList<Double> t;
 	
+	/**
+	 * Costruttore
+	 * @param truckRoute la route del truck
+	 * @param truckSubroutes le subroutes del truck
+	 * @param cprime vettore dei noid che possono essere serviti dall'UAV
+	 * @param t vettore dei tempi di arrivo
+	 */
 	public RouteToUpdate(ArrayList<Node> truckRoute, ArrayList<Subroute> truckSubroutes, ArrayList<Node> cprime,
 			ArrayList<Double> t) {
 		super();
--- FSTSP/src/fstsp/Subroute.java
+++ FSTSP/src/fstsp/Subroute.java
@@ -1,36 +1,51 @@
 package fstsp;
 
 import java.util.ArrayList;
-
+/**
+ * Classe che contiene una subroute e una variabile booleana true se la subroute è servita dall'UAV
+ */
 public class Subroute {
-	private ArrayList<Node> nodes;
-	private boolean UAVserved;
+	private ArrayList<Node> nodes; //nodi della subroute
+	private boolean UAVserved; //true se la subroute è servita dall'UAV
 	
+	/**
+	 * Costruttore
+	 * @param subroute la subroute
+	 * @param uAVserved true se la subroute è servita dall'UAV
+	 */
 	public Subroute(ArrayList<Node> subroute, boolean uAVserved) {
 		super();
 		this.nodes = subroute;
 		UAVserved = uAVserved;
 	}
-	
+	/**
+	 * Costruttore di copia
+	 * @param s elemento da copiare
+	 */
 	@SuppressWarnings("unchecked")
 	public Subroute(ArrayList<Node> s) {
 		nodes  = (ArrayList<Node>) s.clone();
 		UAVserved = false;
 	}
 
-	public ArrayList<Node> getNodes() {
-		return nodes;
-	}
-
-	public boolean isUAVserved() {
-		return UAVserved;
-	}
-
-	public void setNodes(ArrayList<Node> subroute) {
-		this.nodes = subroute;
-	}
-
-	public void setUAVserved(boolean uAVserved) {
-		UAVserved = uAVserved;
-	}
+	/**
+	 * getter per i nodi
+	 * @return i nodi della subroute
+	 */
+	public ArrayList<Node> getNodes() {return nodes;}
+	/**
+	 * getter per UAV served
+	 * @return true se la subroute è servita dall'UAV
+	 */
+	public boolean isUAVserved() {return UAVserved;}
+	/**
+	 * Setter per i nodi
+	 * @param subroute la subroute da inserire
+	 */
+	public void setNodes(ArrayList<Node> subroute) {this.nodes = subroute;}
+	/**
+	 * Setter per UAV served
+	 * @param uAVserved true se la subroute è servita dall'UAV
+	 */
+	public void setUAVserved(boolean uAVserved) {UAVserved = uAVserved;}
 }
